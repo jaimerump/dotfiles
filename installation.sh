8f6dd5f1862b8d995c99e219cb6dca97f459ca9b
Jaime Rump <jaime.lee.rump@gmail.com>
2019-08-08 21:29:39 -0400

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,19 @@
+# Install Homebrew
+# Don't copy multiple lines because the Homebrew installation will ask for password
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+
+# Install git
+brew install git
+
+# Install node
+
+# Install python
+
+# Install mongo
+
+# Install mongo-tools
+
+# Install redis
+
+# 
