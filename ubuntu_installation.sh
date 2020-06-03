4d4eb2b6bb763d68eb8095a43ff2296e38dfb2db
Jaime Rump <jaime.lee.rump@gmail.com>
2020-06-03 17:08:47 -0400

    Install docker from source instead of snap

--- ubuntu_installation.sh
+++ ubuntu_installation.sh
@@ -96,7 +96,23 @@ sudo snap install kubectl --classic
 sudo snap install terraform
 sudo apt-get install virtualbox virtualbox-ext-pack
 sudo snap install kubectl --classic
-sudo snap install docker
+
+# Install docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 # Install docker-compose, couldn't find snap or apt package
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
